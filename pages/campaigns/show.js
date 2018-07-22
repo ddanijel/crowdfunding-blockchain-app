4aeb1bcf03d0601ea87a8d1756a144c7d80c43f3
danijel <danijel.fon@gmail.com>
2018-07-22 13:39:32 +0200

    view request route added

--- pages/campaigns/show.js
+++ pages/campaigns/show.js
@@ -1,10 +1,10 @@
 import React, {Component} from 'react';
 import Layout from '../../components/Layout';
 import Campaign from '../../ethereum/campaign';
-import {Card, Grid} from 'semantic-ui-react';
+import {Card, Grid, Button} from 'semantic-ui-react';
 import web3 from '../../ethereum/web3';
 import ContributeForm from '../../components/ContributeForm';
-imop
+import {Link} from '../../routes';
 
 class CampaignShow extends Component {
     // this is a separate props object than the one that
@@ -69,12 +69,23 @@ class CampaignShow extends Component {
             <Layout>
                 <h3>Campaign Show</h3>
                 <Grid>
-                    <Grid.Column width={10}>
-                        {this.renderCards()}
-                    </Grid.Column>
-                    <Grid.Column width={6}>
-                        <ContributeForm address={this.props.address}/>
-                    </Grid.Column>
+                    <Grid.Row>
+                        <Grid.Column width={10}>
+                            {this.renderCards()}
+                        </Grid.Column>
+                        <Grid.Column width={6}>
+                            <ContributeForm address={this.props.address}/>
+                        </Grid.Column>
+                    </Grid.Row>
+                    <Grid.Row>
+                        <Grid.Column>
+                            <Link route={`/campaigns/${this.props.address}/requests`}>
+                                <a>
+                                    <Button primary>View Request</Button>
+                                </a>
+                            </Link>
+                        </Grid.Column>
+                    </Grid.Row>
                 </Grid>
             </Layout>
         )
