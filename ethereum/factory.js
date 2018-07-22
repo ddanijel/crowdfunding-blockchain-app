cfb5330616d0b0c6c5b8d893957426db2fe8a70e
danijel <danijel.fon@gmail.com>
2018-07-22 10:57:12 +0200

    added additional methods in smart contract, redeploying and the contracts address changed

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -3,7 +3,8 @@ import CampaignFactory from './build/CampaignFactory';
 
 const instance = new web3.eth.Contract(
     JSON.parse(CampaignFactory.interface),
-    '0x44366d574f227d92A97073519050B677Ca3cF227'
+    '0x33968F2b6716879f9c147BE560a2312F7Fb5b443'
+    // '0x44366d574f227d92A97073519050B677Ca3cF227'  == old factory
 );
 
 export default instance;
